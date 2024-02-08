c01974a4ed2702e0a4288ceeedae8acb385cf579
Hannah-Unyolo <unyolohannah@gmail.com>
2024-02-08 22:02:09 +0300

    added data

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val fullname = "Hannah Unyolo"
+    println(fullname)
+    val age = 23
+    println(age)
+    val phoneNumber = "0888863338"
+    println(phoneNumber)
+    val weight = 56.0F
+    println(weight)
+    val isKenyan = false
+    print(isKenyan)
+
+}
